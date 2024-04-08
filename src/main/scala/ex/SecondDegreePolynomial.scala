5fdd5181bd452e6b8a6b8b4013f99d1cd0600e64
zanetti33 <lorenzo.zanetti33@gmail.com>
2024-04-08 13:19:43 +0200

    ex 1 and 2

--- src/main/scala/ex/SecondDegreePolynomial.scala
+++ src/main/scala/ex/SecondDegreePolynomial.scala
@@ -13,16 +13,44 @@ trait SecondDegreePolynomial:
 
 
 object SecondDegreePolynomial:
-  def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial = ??? // Fill here
+  private class SecondDegreePolynomialImpl(override val secondDegree: Double,
+                                           override val firstDegree: Double,
+                                           override val constant: Double) extends SecondDegreePolynomial:
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomialImpl(secondDegree + polynomial.secondDegree,
+        firstDegree + polynomial.firstDegree,
+        constant + polynomial.constant)
+
+    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomialImpl(secondDegree - polynomial.secondDegree,
+        firstDegree - polynomial.firstDegree,
+        constant - polynomial.constant)
+
+  private case class SecondDegreePolynomialImpl2(override val secondDegree: Double,
+                                                 override val firstDegree: Double,
+                                                 override val constant: Double) extends SecondDegreePolynomial:
+    override def +(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomialImpl(secondDegree + polynomial.secondDegree,
+        firstDegree + polynomial.firstDegree,
+        constant + polynomial.constant)
+
+    override def -(polynomial: SecondDegreePolynomial): SecondDegreePolynomial =
+      SecondDegreePolynomialImpl(secondDegree - polynomial.secondDegree,
+        firstDegree - polynomial.firstDegree,
+        constant - polynomial.constant)
+  def apply(secondDegree: Double, firstDegree: Double, constant: Double): SecondDegreePolynomial = SecondDegreePolynomialImpl2(secondDegree, firstDegree, constant) // Fill here
 
 @main def checkComplex(): Unit =
   val simplePolynomial = SecondDegreePolynomial(1.0, 0, 3)
+  val samePolynomial = SecondDegreePolynomial(1.0, 0, 3)
   val anotherPolynomial = SecondDegreePolynomial(0.0, 1, 0.0)
   val fullPolynomial = SecondDegreePolynomial(3.0, 2.0, 5.0)
   val sum = simplePolynomial + anotherPolynomial
   println((sum, sum.secondDegree, sum.firstDegree, sum.constant)) // 1.0 * X^2 + 1.0 * X + 3.0
   val multipleOperations = fullPolynomial - (anotherPolynomial + simplePolynomial)
   println((multipleOperations, multipleOperations.secondDegree, multipleOperations.firstDegree, multipleOperations.constant)) // 2.0 * X^2 + 1.0 * X + 2.0
+  println(simplePolynomial.equals(samePolynomial))
+  println(simplePolynomial.toString)
 
 /** Hints:
   *   - implement SecondDegreePolynomial with a SecondDegreePolynomialImpl class, similar to PersonImpl in slides
--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -8,7 +8,22 @@ trait Item:
   def tags: Sequence[String]
 
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+
+  def apply(code: Int, name: String, tags: String*): Item =
+    var tagSequence = Sequence.empty[String]
+    for i <- 0 until tags.length do tagSequence = tagSequence.concat(Sequence[String](tags.apply(i)))
+    ItemImpl(code, name, tagSequence)
+  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ItemImpl(code, name, tags)
+
+
+  def unapply(item: Item): Option[(Int, String, Sequence[String])] = Some((item.code, item.name, item.tags))
+
+    private case class ItemImpl(override val code: Int,
+                                override val name: String,
+                                override val tags: Sequence[String]) extends Item:
+      override def equals(obj: Any): Boolean = obj match
+        case Item(otherCode, _, _) => otherCode == code
+        case _ => false
 
 /**
  * A warehouse is a place where items are stored.
@@ -42,10 +57,22 @@ trait Warehouse:
    * @return true if the warehouse contains an item with the given code, false otherwise
    */
   def contains(itemCode: Int): Boolean
+
 end Warehouse
 
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(): Warehouse = WarehouseImpl(Sequence.empty)
+
+  private case class WarehouseImpl(var items: Sequence[Item]) extends Warehouse:
+    override def contains(itemCode: Int): Boolean = items.contains(Item(itemCode, null, null))
+
+    override def store(item: Item): Unit = items = items.concat(Sequence(item))
+
+    override def searchItems(tag: String): Sequence[Item] = items.filter(item => item.tags.contains(tag))
+
+    override def retrieve(code: Int): Optional[Item] = items.find(item => item.code == code)
+
+    override def remove(item: Item): Unit = items = items.filter(f = currentItem => currentItem != item)
 
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
@@ -54,18 +81,19 @@ object Warehouse:
   val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
   val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
 
-  warehouse.contains(dellXps.code) // false
+  println(warehouse.contains(dellXps.code)) // false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  println(warehouse.contains(dellXps.code)) // true
   warehouse.store(dellInspiron) // side effect, add dell Inspiron to the warehouse
   warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
-  warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
-  warehouse.searchItems("notebook") // Sequence(dellXps, dell Inspiron)
-  warehouse.retrieve(11) // None
-  warehouse.retrieve(dellXps.code) // Just(dellXps)
+  println(warehouse.searchItems("mobility")) // Sequence(xiaomiMoped)
+  println(warehouse.searchItems("notebook")) // Sequence(dellXps, dell Inspiron)
+  println(warehouse.retrieve(11)) // None
+  println(warehouse.retrieve(dellXps.code)) // Just(dellXps)
   warehouse.remove(dellXps) // side effect, remove dell xps from the warehouse
-  warehouse.retrieve(dellXps.code) // None
+  println(warehouse.retrieve(dellXps.code)) // None
 
+  println(Item(17, "Prodotto", "l", "zanetti33", "xl"))
 /** Hints:
  * - Implement the Item with a simple case class
  * - Implement the Warehouse keeping a private List of items
